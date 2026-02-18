babccf25760494a4cc70d572a863100a3f6f0613
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2026-02-18 14:22:48 +0530

    Update operatest.ps1

--- operatest.ps1
+++ operatest.ps1
@@ -1,5 +1,6 @@
 $OperaUrl = "https://net.geo.opera.com/opera/stable/windows"
 $InstallerPath = "C:\Temp\OperaSetup.exe"
+$LogFile = "C:\Temp\OperaUpdate.log
 
 if (-not (Test-Path "C:\Temp")) {
     New-Item -ItemType Directory -Path "C:\Temp" | Out-Null
@@ -9,6 +10,6 @@ Write-Host "Downloading Opera..."
 Start-BitsTransfer -Source $OperaUrl -Destination $InstallerPath
 
 Write-Host "Installing / updating Opera..."
-Start-Process -FilePath $InstallerPath -ArgumentList "/silent /allusers=1 language=en /launchbrowser=0" 
+Start-Process -FilePath $InstallerPath -ArgumentList "/silent /allusers=1 /language=en /launchbrowser=0" -RedirectStandardOutput $LogFile -RedirectStandardError $LogFile 
 
-Write-Host "Done."
+Write-Host "Check after 5 minutes and it is installed and check"
